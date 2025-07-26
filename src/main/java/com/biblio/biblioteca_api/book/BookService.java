1b8e14b199568a3ea0c1c55c786d6ea3a9633da2
icaro.goncalves <icarolima1348@gmail.com>
2025-07-26 10:29:13 -0300

    chore: adding Transactional annotation for methods update and create.

--- src/main/java/com/biblio/biblioteca_api/book/BookService.java
+++ src/main/java/com/biblio/biblioteca_api/book/BookService.java
@@ -13,7 +13,7 @@ public class BookService {
 
     private final BookRepository bookRepo;
 
-
+    @Transactional
     public Book create(Book book){
         bookRepo.findByIsbn(book.getIsbn())
                 .ifPresent(b ->{throw new IllegalArgumentException("The inserted book is already cataloged." + book.getIsbn()); });
@@ -25,7 +25,7 @@ public class BookService {
         return bookRepo.findAll(pageable);
     }
 
-
+    @Transactional
     public Book update(UUID id, Book updatedBook){
         Book existing = bookRepo.findById(id)
                 .orElseThrow(()-> new IllegalArgumentException("Book not found:" + id));
