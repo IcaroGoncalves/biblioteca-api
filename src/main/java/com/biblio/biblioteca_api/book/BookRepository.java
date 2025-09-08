edeb9c70bc5a2968084a5ce1693ad98205c51bf1
icaro.goncalves <icarolima1348@gmail.com>
2025-09-07 23:24:10 -0300

    feat: Adding Speficiation to BookRepository.

--- src/main/java/com/biblio/biblioteca_api/book/BookRepository.java
+++ src/main/java/com/biblio/biblioteca_api/book/BookRepository.java
@@ -1,9 +1,11 @@
 package com.biblio.biblioteca_api.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import java.util.Optional;
 import java.util.UUID;
 
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface BookRepository extends JpaRepository<Book, UUID>, JpaSpecificationExecutor<Book> {
     Optional<Book> findByIsbn(String isbn);
 }
